ddbbdb7a482f351c9f1642a33caa60159173ec57
badubizzle <badu.boahen@gmail.com>
2020-11-08 16:14:39 -0600

    Add DAG store config

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,10 @@ config :ex_dag, ExDag.Repo,
   hostname: "localhost"
 
 config :ex_dag,
+  store_adapter: ExDag.Store.FileStore,
+  store_adapter_options: [dags_path: "priv/dags"]
+
+config :ex_dag,
   ecto_repos: [ExDag.Repo]
 
 if Mix.env() == :dev do
