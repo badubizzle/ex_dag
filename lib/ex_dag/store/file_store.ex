acf938f74b8ae9a2013a9c1ebf7858dc671bb214
badubizzle <badu.boahen@gmail.com>
2020-11-08 16:14:09 -0600

    Update DAG Store functions

--- lib/ex_dag/store/file_store.ex
+++ lib/ex_dag/store/file_store.ex
@@ -13,16 +13,25 @@ defmodule ExDag.Store.FileStore do
   end
 
   @impl true
-  def save_dag_run(options, %{dag_id: dag_id}=dag) do
+  def get_dag_path(options, dag) do
     dags_path = get_dags_path(options)
-    runs_paths = Path.join([dags_path, "runs", dag_id])
-
+    file_name = "dag_file_#{dag.dag_id}"
+    {:ok, Path.join([dags_path, file_name])}
   end
 
   @impl true
-  def get_dags(options) when is_list(options) do
+  def save_dag_run(options, dag_run) do
+    dags_path = get_dags_path(options)
+    dag = dag_run.dag
+    runs_path = Path.join([dags_path, "runs", dag.dag_id])
+    File.mkdir_p(runs_path)
+    path = Path.join([runs_path, dag_run.id])
+    File.write(path, :erlang.term_to_binary(dag_run), [:write])
+  end
+
+  @impl true
+  def get_dags(options) do
     dags_path = Keyword.get(options, :dags_path)
-    IO.inspect(dags_path)
 
     case File.dir?(dags_path) do
       true ->
@@ -31,14 +40,30 @@ defmodule ExDag.Store.FileStore do
           dag =
             File.read!(Path.join([dags_path, path]))
             |> :erlang.binary_to_term()
-            {dag.dag_id, dag}
+
+          {dag.dag_id, dag}
         end)
         |> Map.new()
     end
   end
 
-  def get_dag_runs(dag_path, dag_id) do
-    runs_path = Path.join([dag_path, dag_id, "runs"])
+  def get_dag_runs(options, dag) do
+    dags_path = get_dags_path(options)
+    runs_path = Path.join([dags_path, "runs", dag.dag_id])
+
+    if File.dir?(runs_path) do
+      File.ls!(dags_path)
+      |> Enum.map(fn path ->
+        dag_run =
+          File.read!(Path.join([dags_path, path]))
+          |> :erlang.binary_to_term()
+
+        {dag_run.id, dag_run}
+      end)
+      |> Map.new()
+    else
+      %{}
+    end
   end
 
   defp get_dags_path(options) do
--- lib/ex_dag/store/store.ex
+++ lib/ex_dag/store/store.ex
@@ -1,41 +1,64 @@
 defmodule ExDag.Store.Adapter do
-
-  @callback save_dag(Keyword.t, ExDag.DAG.t) :: :ok | {:error, atom()}
-  @callback save_dag_run(Keyword.t, ExDag.DAGRun.t) :: :ok | {:error, atom()}
-  @callback get_dags(Keyword.t) :: map()
+  @moduledoc """
+  Store Adapter Interface/Behaviour
+  """
+  @callback save_dag(options :: Keyword.t(), dag :: ExDag.DAG.t()) :: :ok | {:error, atom()}
+  @callback get_dag_path(options :: Keyword.t(), dag :: ExDag.DAG.t()) ::
+              {:ok, binary()} | {:error, binary()}
+  @callback save_dag_run(options :: Keyword.t(), dag_run :: ExDag.DAGRun.t()) ::
+              :ok | {:error, atom()}
+  @callback get_dags(Keyword.t()) :: map()
 end
 
 defmodule ExDag.Store do
-
+  @moduledoc """
+  Store Module
+  """
+  @spec get_adapter :: atom()
   def get_adapter() do
     Application.get_env(:ex_dag, :store_adapter, ExDag.Store.FileStore)
   end
 
+  @spec get_adapter_options :: Keyword.t()
   def get_adapter_options() do
     Application.get_env(:ex_dag, :store_adapter_options, [])
+  end
+
+  @spec get_dag_path(ExDag.DAG.t()) :: {:ok, binary()} | {:error, binary()}
+  def get_dag_path(dag) do
+    call(:get_dag_path, dag)
   end
 
   def get_dags() do
     call(:get_dags)
   end
 
-  def save_dag(dag) do
+  def get_dag_runs() do
+  end
+
+  def get_dag_runs(dag) do
+    call(:get_dag_runs, dag)
+  end
+
+  def save_dag(%ExDag.DAG{} = dag) do
     call(:save_dag, dag)
   end
 
-  def save_dag_run(dag) do
-    call(:save_dag_run, dag)
+  def save_dag_run(dag_run) do
+    call(:save_dag_run, dag_run)
   end
 
+  @spec call(f :: atom()) :: any()
   defp call(f) when is_atom(f) do
     adapter = get_adapter()
     options = get_adapter_options()
     apply(adapter, f, [options])
   end
 
-  defp call(f, opts) when is_atom(f) do
+  @spec call(f :: atom(), args :: any()) :: any()
+  defp call(f, args) when is_atom(f) do
     adapter = get_adapter()
-    options = get_adapter_options()
-    apply(adapter, f, [options | opts])
+    options = Keyword.new(get_adapter_options())
+    apply(adapter, f, [options, args])
   end
 end
