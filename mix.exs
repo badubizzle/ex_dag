e81618b93ec37583605774a9b390a89b5904d014
Badu Boahen <badu.boahen@gmail.com>
2021-04-18 01:03:30 -0500

    Update swarm version. Add husky

--- mix.exs
+++ mix.exs
@@ -27,13 +27,14 @@ defmodule ExDag.MixProject do
       {:libgraph, "~> 0.7"},
       {:table_rex, "~> 3.0.0"},
       {:libcluster, "~> 3.2"},
-      {:swarm, "~> 3.0"},
+      {:swarm, "~> 3.4"},
       {:phoenix_pubsub, "~> 2.0"},
       {:jason, "~> 1.2"},
       {:ecto_sql, "~> 3.0"},
       {:postgrex, ">= 0.0.0"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
+      {:husky, "~> 1.0", only: :dev, runtime: false}
     ]
   end
 end
